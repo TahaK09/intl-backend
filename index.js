567801be3bed5075264a803ebe742a847a182b3d
TahaK09 <114945783+TahaK09@users.noreply.github.com>
2024-11-06 19:52:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ app.get("/api/links", async (req, res) => {
 });
 
 
-
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
